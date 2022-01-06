65effe95a0f30d0965f8dbc14e80f4f0ae1b8451
fundamental-michele <Michele.Dandrea@fundamentalmedia.com>
2022-01-06 17:12:07 +0000

    first setup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # action-update-post-merge
+
+```bash
+name: Sync multiple branches
+on:
+  push:
+    branches:
+      - main/staging
+jobs:
+  sync-branch:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+
+      - name: Merge main/staging -> main/beta
+        uses: FundamentalMedia/action-update-post-merge@v1.1
+        with:
+          from_branch: main/staging
+          target_branch: main/beta
+          github_token: ${{ github.token }}
+
+```
---
+++ action.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const from_branch = core.getInput('from_branch');
+    const target_branch = core.getInput('target_branch');
+    const octokit = github.getOctokit(GITHUB_TOKEN); 
+    
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    
+    const owner = context.repo.owner
+    const repo  = context.repo.repo
+
+    console.log("Repo info", context.repo)
+
+    console.log("updating target branch", target_branch)
+    console.log("with head branch", from_branch)
+    await octokit.rest.repos.merge({
+        owner,
+        repo,
+        base: target_branch,
+        head: from_branch,
+        commit_message: "update automatically secondary branch with action"
+      });
+
+}
+
+run().catch(err => core.setFailed(err.message));
